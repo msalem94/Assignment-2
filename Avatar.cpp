8326344ecf21d456cdbcd9fb8540a84e00d0073d
msalem94 <marwansalem1994@hotmail.com>
2015-11-14 19:37:58 -0800

    added avatar class

---
+++ Avatar.cpp
@@ -0,0 +1,100 @@
+/*
+* Implementation of the Avatar class.
+*/
+
+#include "Avatar.h"
+
+/*
+* Base constructor for the Avatar class
+*/
+Avatar::Avatar(const std::string &n, const unsigned &l, const ClassType t) :
+Actor(),
+_name(n),
+_level(l),
+_health(l * 3),
+_class(t)
+{
+	_sprite_image.create(10, 10, sf::Color::Red);
+	_sprite_texture.loadFromImage(_sprite_image);
+	_sprite = sf::Sprite(_sprite_texture);
+}
+
+/*
+* Copy constructor for the Avatar class
+*/
+Avatar::Avatar(const Avatar &a) :
+Actor(),
+_name(a.GetName()),
+_level(a.GetLevel()),
+_health(a.GetHealth()),
+_class(a.GetClass()),
+_sprite_image(a._sprite_image)
+{
+	_sprite_texture.loadFromImage(_sprite_image);
+	_sprite = sf::Sprite(_sprite_texture);
+}
+
+/*
+* Returns the Avatar's name
+*/
+std::string Avatar::GetName() const
+{
+	return _name;
+}
+
+/*
+* Returns the Avatar's level
+*/
+unsigned Avatar::GetLevel() const
+{
+	return _level;
+}
+
+/*
+* Returns the Avatar's health
+*/
+unsigned Avatar::GetHealth() const
+{
+	return _health;
+}
+
+/*
+* Returns the Avatar's class
+*/
+AvatarClass Avatar::GetClass() const
+{
+	return _class;
+}
+
+/*
+* Deals damage taken while in match
+*/
+void Avatar::TakeDamage(const unsigned &damage)
+{
+	if (damage > _health)
+	{
+		_health = 0;
+	}
+	else
+	{
+		_health = _health - damage;
+	}
+}
+
+/*
+* Position override that also sets the sprite position.
+*/
+void Avatar::SetPosition(const float &xPosition, const float &yPosition)
+{
+	sf::Vector2f pos(xPosition, yPosition);
+	_position = pos;
+	_sprite.setPosition(pos);
+};
+
+/*
+* The draw implementation for the Avatar class. Draws the Avatar's sprite.
+*/
+void Avatar::Draw(sf::RenderWindow &window)
+{
+	window.draw(_sprite);
+}
---
+++ Avatar.h
@@ -0,0 +1,62 @@
+/*
+* Declaration of the Avatar class.
+* An Avatar extends Actor. It contains character attributes that are used by human and AI players.
+*/
+#pragma once
+
+#include "Actor.h"
+#include "AvatarClass.h"
+
+class Avatar : public Actor
+{
+public:
+
+	Avatar(const std::string &n, const unsigned &l, const ClassType t);
+	Avatar(const Avatar &a);
+	~Avatar() {}
+
+	/*
+	* Returns the Avatar's name
+	*/
+	std::string GetName() const;
+
+	/*
+	* Returns the Avatar's level
+	*/
+	unsigned GetLevel() const;
+
+	/*
+	* Returns the Avatar's health
+	*/
+	unsigned GetHealth() const;
+
+	/*
+	* Returns the Avatar's class object
+	*/
+	AvatarClass GetClass() const;
+
+	/*
+	* Deals damage taken while in match
+	*/
+	void TakeDamage(const unsigned &damage);
+
+	/*
+	* Start Actor Interface
+	*/
+	void Draw(sf::RenderWindow &window) override;
+
+	void SetPosition(const float &xPosition, const float &yPosition) override;
+	/*
+	* End Actor Interface
+	*/
+
+protected:
+
+	const std::string _name;
+	unsigned _level;
+	unsigned _health;
+	sf::Sprite _sprite;
+	sf::Image _sprite_image;
+	sf::Texture _sprite_texture;
+	const AvatarClass _class;
+};
